1b5b85d1d0d242d4c40fc27badc99d0f15a19515
Danil Nikolenko <anttarras099@gmail.com>
2020-08-25 11:36:18 +0300

    Add main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"UserDataTestTask/config"
+	"UserDataTestTask/server"
+	"github.com/labstack/gommon/log"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	if err := config.Init(); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	app := server.NewApp()
+
+	if err := app.Run(":" + viper.GetString("port")); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+}
