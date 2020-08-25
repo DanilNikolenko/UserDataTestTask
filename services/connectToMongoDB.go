001cbf3115ae8f0e52ab8ff5776a62c4d8e91398
Danil Nikolenko <anttarras099@gmail.com>
2020-08-25 11:35:23 +0300

    Add connectToMongo in services

---
+++ services/connectToMongoDB.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnToMongo() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create connect
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
